e3eb9816d05d15b1a4927a42269b70af018520b8
mikoman <mankonikos@gmail.com>
2016-05-21 04:59:37 +0100

    First Commit

--- Downloads/socket.io-master/examples/chat/index.js
+++ Downloads/socket.io-master/examples/chat/index.js
@@ -3,9 +3,11 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('../..')(server);
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 3000;
 
-server.listen(port);
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+});
 
 // Routing
 app.use(express.static(__dirname + '/public'));
